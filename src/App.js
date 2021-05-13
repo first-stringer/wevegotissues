0314c26213bb6bbc7260903273212a3d13e8d9d8
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-05-13 05:44:19 -0500

    parameterize list function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
 import React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-}, {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-}, ];
-
 const welcome = {
   greeting: 'Hey',
   title: 'React',
@@ -27,8 +10,8 @@ function getTitle(title) {
   return title;
 }
 
-const List = () =>
-  list.map(item => 
+const List = props =>
+  props.list.map(item => 
     <div key={item.objectID}>
       <span>
         <a href={item.url}>{item.title}</a>
@@ -39,7 +22,25 @@ const List = () =>
     </div> 
   )
 
-function App() {
+  const App = () => {
+    const stories = [
+      {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      }, {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+      }, 
+    ];
+
   const handleChange = event => {
     console.log(event.target.value);
   };
@@ -58,9 +59,7 @@ function App() {
       <input id="search" type="text" onChange={handleChange} />
 
       <hr />
-      <List />
-      <hr />
-      <List />
+      <List list={stories} />
 
     </div> 
   );
