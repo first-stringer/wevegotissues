c60ad8973cd1fd433881905ad1fd1ad4b08fcd7a
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-05-13 05:27:43 -0500

    omit the function statement, the curly braces, and the return statement from list function

--- src/App.js
+++ src/App.js
@@ -27,20 +27,17 @@ function getTitle(title) {
   return title;
 }
 
-function List() {
-  return list.map(function(item) {
-    return (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span> {item.num_comments}</span>
-        <span> {item.points}</span>
-      </div> 
-    );
-  }); 
-}
+const List = () =>
+  list.map(item => 
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span> {item.num_comments}</span>
+      <span> {item.points}</span>
+    </div> 
+  )
 
 function App() {
   return (
