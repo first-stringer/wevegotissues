83e7181d1294830fa136ae607c6d96aa50da7401
Justin Michael Stringer <first-stringer@users.noreply.github.com>
2021-05-12 20:10:23 -0500

    more practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
-const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <h1>
+        Hello {getTitle('React')}
+      </h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div> 
   );
 }
